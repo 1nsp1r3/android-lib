6c372f45ba8b0e5e534cf100aa609f1157437fc8
1nsp1r3 <abel.andouard@gmail.com>
2024-07-16 01:02:27 +0200

    Add switchActivity to I3 class

--- org/inspir3/common/I3.kt
+++ org/inspir3/common/I3.kt
@@ -1,5 +1,8 @@
 package org.inspir3.common
 
+import android.content.Context
+import android.content.Intent
+
 /**
  * DO NOT EDIT
  * See android-lib project
@@ -7,5 +10,10 @@ package org.inspir3.common
 class I3 {
     companion object {
         const val TAG = "Inspir3"
+
+        inline fun <reified T : Any> switchToActivity(context: Context, klass: Class<T>) {
+            val intent = Intent(context, klass)
+            context.startActivity(intent)
+        }
     }
 }
