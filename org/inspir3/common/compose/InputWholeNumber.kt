c2be10215f6cea1d16cdb221aa18b71af0c312ad
1nsp1r3 <abel.andouard@gmail.com>
2024-07-20 16:12:24 +0200

    Improve InputWholeNumber.kt

--- org/inspir3/common/compose/InputWholeNumber.kt
+++ org/inspir3/common/compose/InputWholeNumber.kt
@@ -4,38 +4,36 @@
  */
 package org.inspir3.common.compose
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
-
-class InputWholeNumberState {
-    var value: Long by mutableLongStateOf(0)
-}
 
 @Composable
 fun InputWholeNumber(
     label: String,
-    inputWholeNumberState: InputWholeNumberState,
+    value: MutableState<String>,
+    onUpdate: (Long) -> Unit,
 ) {
-    var text by remember { mutableStateOf(inputWholeNumberState.value.toString()) }
-    var isError by remember { mutableStateOf(false) }
+    var text by rememberSaveable { value }
+    var isError by rememberSaveable { mutableStateOf(false) }
 
     fun validate(text: String) {
         if (text.matches(Regex("^[0-9]+$"))) {
-            inputWholeNumberState.value = text.toLong()
+            onUpdate(text.toLong())
             isError = false
         } else {
-            inputWholeNumberState.value = 0
+            onUpdate(0)
             isError = true
         }
     }
